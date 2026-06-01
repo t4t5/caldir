eb71600b55cd5829b435c8808594038d81f8822f
Tristan (t4t5) <t4t5@hey.com>
2026-06-01 15:33:31 +0100

    Remove caldir-core-old

--- caldir-core-old/src/ics/mod.rs
+++
@@ -1,9 +0,0 @@
-//! ICS file generation and parsing.
-//!
-//! This module handles reading and writing .ics files according to RFC 5545.
-
-mod generate;
-mod parse;
-
-pub use generate::generate_ics;
-pub use parse::{parse_event, parse_events};
