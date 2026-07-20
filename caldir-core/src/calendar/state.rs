805446d003da5da3a63e779306d826b6170f1679
Tristan (t4t5) <t4t5@hey.com>
2026-07-20 11:00:52 +0100

    Create sync_bases file

--- caldir-core/src/calendar/state.rs
+++ caldir-core/src/calendar/state.rs
@@ -1,102 +1,29 @@
 mod error;
 mod event_bases;
 mod known_event_ids;
+mod sync_bases;
 
-use crate::{Event, calendar::state::event_bases::EventBases, event::EventInstanceId};
 pub use error::CalendarStateError;
-use known_event_ids::KNOWN_IDS_FILE_NAME;
-use std::{
-    collections::{HashMap, HashSet},
-    path::Path,
-};
+use std::path::Path;
 
-pub(crate) use known_event_ids::KnownEventIds;
+use sync_bases::SyncBases;
 
 #[derive(Debug)]
 pub struct CalendarState {
-    sync_bases: HashMap<EventInstanceId, SyncBase>,
-}
-
-#[derive(Debug)]
-enum SyncBase {
-    KnownEventId,          // legacy "known_event_ids" file
-    EventBase(Box<Event>), // new format for 3-way sync
+    sync_bases: SyncBases,
 }
 
 impl CalendarState {
     pub fn new() -> Self {
         Self {
-            sync_bases: HashMap::new(),
+            sync_bases: SyncBases::new(),
         }
     }
 
     pub fn load(state_dir: &Path) -> Result<Self, CalendarStateError> {
-        let known_event_ids = Self::load_known_event_ids(state_dir)?;
-        let event_bases = Self::load_event_bases(state_dir)?;
-
-        let mut sync_bases = known_event_ids
-            .iter()
-            .map(|id| (id.clone(), SyncBase::KnownEventId))
-            .collect::<HashMap<_, _>>();
-
-        for (id, event) in event_bases {
-            sync_bases.insert(id, SyncBase::EventBase(Box::new(event)));
-        }
+        let sync_bases = SyncBases::load_from_state_dir(state_dir)?;
 
         Ok(Self { sync_bases })
-    }
-
-    fn load_known_event_ids(state_dir: &Path) -> Result<KnownEventIds, CalendarStateError> {
-        let known_ids_path = state_dir.join(KNOWN_IDS_FILE_NAME);
-        KnownEventIds::load(&known_ids_path)
-    }
-
-    fn load_event_bases(state_dir: &Path) -> Result<EventBases, CalendarStateError> {
-        let event_bases_dir = state_dir.join(event_bases::EVENT_BASES_DIR_NAME);
-        let event_bases = event_bases::EventBases::load(&event_bases_dir)?;
-        Ok(event_bases)
-    }
-
-    pub(crate) fn synced_event_ids(&self) -> HashSet<EventInstanceId> {
-        self.sync_bases
-            .iter()
-            .map(|(id, sync_base)| match sync_base {
-                SyncBase::KnownEventId => id.clone(),
-                SyncBase::EventBase(event) => event.event_instance_id(),
-            })
-            .collect()
-    }
-
-    pub(crate) fn known_event_ids(&self) -> KnownEventIds {
-        let ids = self
-            .sync_bases
-            .iter()
-            .filter(|&(_id, sync_base)| matches!(sync_base, SyncBase::KnownEventId))
-            .map(|(id, _sync_base)| id.clone())
-            .collect();
-
-        KnownEventIds::from(ids)
-    }
-
-    // Todo: change to "save"? and store event bases too?
-    pub fn write(&self, state_dir: &Path) -> Result<(), CalendarStateError> {
-        std::fs::create_dir_all(state_dir)?;
-
-        let known_ids_path = state_dir.join(KNOWN_IDS_FILE_NAME);
-
-        self.known_event_ids().write(&known_ids_path)?;
-
-        Ok(())
-    }
-
-    pub(crate) fn add_new_synced_ids(
-        &mut self,
-        ids: impl IntoIterator<Item = EventInstanceId>,
-    ) -> &mut Self {
-        for id in ids {
-            self.sync_bases.entry(id).or_insert(SyncBase::KnownEventId);
-        }
-        self
     }
 }
 
@@ -111,44 +38,6 @@ mod tests {
         std::fs::write(dir.path().join(KNOWN_IDS_FILE_NAME), "abc@hooli.com").unwrap();
 
         CalendarState::load(dir.path()).unwrap();
-    }
-
-    #[test]
-    fn event_bases_take_priority_over_known_event_ids() {
-        let dir = tempfile::TempDir::new().unwrap();
-        std::fs::write(
-            dir.path().join(KNOWN_IDS_FILE_NAME),
-            "base@caldir\nfallback@caldir",
-        )
-        .unwrap();
-        let bases_dir = dir.path().join(event_bases::EVENT_BASES_DIR_NAME);
-        std::fs::create_dir(&bases_dir).unwrap();
-        std::fs::write(
-            bases_dir.join("base.ics"),
-            "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nBEGIN:VEVENT\r\nUID:base@caldir\r\nDTSTART:20240101T120000Z\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n",
-        )
-        .unwrap();
-
-        let state = CalendarState::load(dir.path()).unwrap();
-
-        assert!(matches!(
-            state.sync_bases.get(&EventInstanceId::from("base@caldir")),
-            Some(SyncBase::EventBase(_))
-        ));
-        assert!(matches!(
-            state
-                .sync_bases
-                .get(&EventInstanceId::from("fallback@caldir")),
-            Some(SyncBase::KnownEventId)
-        ));
-        assert_eq!(
-            state
-                .known_event_ids()
-                .iter()
-                .cloned()
-                .collect::<HashSet<_>>(),
-            HashSet::from([EventInstanceId::from("fallback@caldir")])
-        );
     }
 
     #[test]
---
+++ caldir-core/src/calendar/state/sync_bases.rs
@@ -0,0 +1,75 @@
+use crate::{Event, event::EventInstanceId};
+use std::collections::HashMap;
+use std::path::Path;
+
+use super::error::CalendarStateError;
+use super::event_bases::{EVENT_BASES_DIR_NAME, EventBases};
+use super::known_event_ids::{KNOWN_IDS_FILE_NAME, KnownEventIds};
+
+// If event base file exists -> <EventInstanceId, Some<Event>>
+// If no event base file, but known event ID exists -> <EventInstanceId, None>
+#[derive(Debug)]
+pub struct SyncBases(HashMap<EventInstanceId, Option<Box<Event>>>);
+
+impl SyncBases {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn insert_known_event_id(&mut self, id: EventInstanceId) {
+        self.0.insert(id, None);
+    }
+
+    pub fn insert_event_base(&mut self, id: EventInstanceId, event: Event) {
+        self.0.insert(id, Some(Box::new(event)));
+    }
+
+    pub fn get(&self, id: &EventInstanceId) -> Option<&Option<Box<Event>>> {
+        self.0.get(id)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&EventInstanceId, &Option<Box<Event>>)> {
+        self.0.iter()
+    }
+
+    pub fn load_from_state_dir(state_dir: &Path) -> Result<Self, CalendarStateError> {
+        let known_event_ids = Self::load_known_event_ids(state_dir)?;
+        let event_bases = Self::load_event_bases(state_dir)?;
+
+        let sync_bases = Self::from_event_bases_and_known_ids(event_bases, known_event_ids);
+
+        Ok(sync_bases)
+    }
+
+    // Legacy file:
+    fn load_known_event_ids(state_dir: &Path) -> Result<KnownEventIds, CalendarStateError> {
+        let known_ids_path = state_dir.join(KNOWN_IDS_FILE_NAME);
+        KnownEventIds::load(&known_ids_path)
+    }
+
+    // New file format with event bases:
+    fn load_event_bases(state_dir: &Path) -> Result<EventBases, CalendarStateError> {
+        let event_bases_dir = state_dir.join(EVENT_BASES_DIR_NAME);
+        let event_bases = EventBases::load(&event_bases_dir)?;
+        Ok(event_bases)
+    }
+
+    // Use event bases if they exist,
+    // Fallback to known event IDs if no event base exists:
+    fn from_event_bases_and_known_ids(
+        event_bases: EventBases,
+        known_event_ids: KnownEventIds,
+    ) -> Self {
+        let mut sync_bases = HashMap::new();
+
+        for id in known_event_ids.iter() {
+            sync_bases.insert(id.clone(), None);
+        }
+
+        for (id, event) in event_bases.into_iter() {
+            sync_bases.insert(id, Some(Box::new(event)));
+        }
+
+        Self(sync_bases)
+    }
+}
